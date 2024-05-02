194c243063c45bf5a0563da447f0c6da6604c422
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-05-02 12:27:46 +0200

    created nav for above small screens

--- src/content/Navbar/index.tsx
+++ src/content/Navbar/index.tsx
@@ -1,35 +1,49 @@
 import { Bars3BottomRightIcon } from "@heroicons/react/24/solid"
 import useMediaQuery from "../../hooks/useMediaQuery"
+import { SelectedPage } from "../../shared/types"
+import { useState } from "react"
+import Link from "./Link"
 
-type Props = {}
+type Props = {
+  isTopOfPage: boolean
+  selectedPage: SelectedPage
+  setSelectedPage: (value: SelectedPage) => void
+}
 
-const Navbar = (props: Props) => {
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between"
   const isAboveSmallScreens = useMediaQuery("(min-width: 768px)")
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false)
+  const navbarBackground = isTopOfPage ? "" : "bg-secondary-100 drop-shadow"
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 w-full py-6 bg-primary-200`}>
-        {isAboveSmallScreens ? (
-          <>
-            <div>
-              <p className="font-bold">ZZ</p>
-            </div>
-
-            <div>
-              <p>Meny</p>
-            </div>
-            <div>
-              <p>Bordbestilling</p>
-            </div>
-            <div>
-              <p>Om oss</p>
-            </div>
-          </>
-        ) : (
-          <div>
-            <Bars3BottomRightIcon className="h-6 w-6" />
+      <div className={`${navbarBackground} ${flexBetween} fixed top-0 w-full py-6 bg-primary-200`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            <Link page="ZZ" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            {isAboveSmallScreens ? (
+              <div className={`${flexBetween} w-full`}>
+                <div></div>
+                <div className={`${flexBetween} gap-8`}>
+                  <Link page="Meny" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                  <Link page="Bord" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                  <Link
+                    page="Om Oss"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+              </div>
+            ) : (
+              <button
+                className="rounded-full bg-secondary-500 p-2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3BottomRightIcon className="h-6 w-6 text-white" />
+              </button>
+            )}
           </div>
-        )}
+        </div>
       </div>
     </nav>
   )
